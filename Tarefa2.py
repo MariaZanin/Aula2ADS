20516587f38969fce1531dbdb5a65004f58d6a2a
MariaZanin <123608066+MariaZanin@users.noreply.github.com>
2023-02-08 23:55:34 +0000

    Aula2ADS

---
+++ Tarefa2.py
@@ -0,0 +1,12 @@
+#Tarefa2
+
+nome = input("Digite seu nome: ")
+ender = input("Digite seu endereço: ")
+cep = input("Digite seu cep: ")
+tel = input("Digite o número do seu telefone: ")
+
+#Dados
+print ("")
+print ("Nome:", nome)
+print ("Endereço: ",ender)
+print ("CEP e Telefone: ", cep,"-", tel)
